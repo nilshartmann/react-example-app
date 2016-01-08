87a6b1b8062c2931e2b4e6989978502ed905141c
Nils Hartmann <nils@nilshartmann.net>
2016-01-08 07:44:21 +0100

    Clean playground to start from beginning

--- src/app.js
+++ src/app.js
@@ -1,8 +1,2 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function Hello() {
-  return <div>Hello World!</div>;
-}
-
-ReactDOM.render(<Hello />, document.getElementById('mount'));
